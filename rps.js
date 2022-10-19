5feabc59804334fdf4ff7cc4be212b53b15228ce
Taylor T Hight <Taylorthight@gmail.com>
2022-10-19 08:20:19 -0800

    Created 'playRound' function

--- rps.js
+++ rps.js
@@ -9,3 +9,35 @@ function getComputerChoice() {
     }
 }
 
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+
+    if ((playerSelection === "Rock") && (computerSelection === "Scissors")) { return "You Win! Rock beats Scissors."; 
+    }
+
+    else if ((playerSelection === "Paper") && (computerSelection === "Scissors")) { return ("You Lose! Scissors beats Paper."); 
+    }
+
+    else if ((playerSelection === "Scissors") && (computerSelection === "Scissors")) { return ("It's a draw!"); 
+    }
+
+    else if ((playerSelection === "Rock") && (computerSelection === "Paper")) { return ("You Lose! Paper beats Rock."); 
+    }
+
+    else if ((playerSelection === "Paper") && (computerSelection === "Paper")) { return ("It's a draw!"); 
+    }
+
+    else if ((playerSelection === "Scissors") && (computerSelection === "Paper")) { return ("You Win! Scissors beats Paper."); 
+    }
+
+    else if ((playerSelection === "Rock") && (computerSelection === "Rock")) { return ("It's a draw!"); 
+    }
+
+    else if ((playerSelection === "Paper") && (computerSelection === "Rock")) { return ("You Win! Paper beats Rock."); 
+    }
+
+    else if ((playerSelection === "Scissors") && (computerSelection === "Rock")) { return ("You Lose! Rock beats Scissors."); 
+    }
+}
+
